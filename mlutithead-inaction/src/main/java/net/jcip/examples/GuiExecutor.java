dca196613eba2b729a60d4c773abb50190615f32
一剑看风云 <chenwei182729@163.com>
2021-01-10 11:34:55 +0800

    并发实战
    java并发编程实战官方源码

---
+++ mlutithead-inaction/src/main/java/net/jcip/examples/GuiExecutor.java
@@ -0,0 +1,58 @@
+package net.jcip.examples;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * GuiExecutor
+ * <p/>
+ * Executor built atop SwingUtilities
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class GuiExecutor extends AbstractExecutorService {
+    // Singletons have a private constructor and a public factory
+    private static final GuiExecutor instance = new GuiExecutor();
+
+    private GuiExecutor() {
+    }
+
+    public static GuiExecutor instance() {
+        return instance;
+    }
+
+    @Override
+    public void execute(Runnable r) {
+        if (SwingUtilities.isEventDispatchThread()) {
+            r.run();
+        } else {
+            SwingUtilities.invokeLater(r);
+        }
+    }
+
+    @Override
+    public void shutdown() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<Runnable> shutdownNow() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit)
+            throws InterruptedException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return false;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return false;
+    }
+}
